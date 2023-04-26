ffa8e3ed5afb0b8e7a450db3b3e6d3775503344c
theacademydvr <theacademy.dvr@gmail.com>
2023-04-25 23:31:39 -0500

    cambios en la casa de dashboard 25/04/2023

--- src/App.js
+++ src/App.js
@@ -12,8 +12,8 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home />}></Route>
-        <Route path='/signup' element={<Signup />}></Route>
         <Route path='/login' element={<Login />}></Route>
+        <Route path='/signup' element={<Signup />}></Route>
         <Route path='/dashboard' element={<Dashboard />}></Route>
       </Routes>
     </BrowserRouter>
